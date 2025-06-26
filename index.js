b58811ca8851f0411bc53b9141aa4646f27e5c1a
Taebenu Relin <S22210152@Student.unklab.ac.id>
2025-06-26 18:36:25 +0800

    FinalMAD

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+
 import {name as appName} from './app.json';
+import Flexbox from './Flexbox';
+import Login from './Login';
+import RestApi from './RestApi';
 
 AppRegistry.registerComponent(appName, () => App);
